abfaccf2fcf636e1b878e6463f075f7aea0d335c
Robolightning <89173189+Robolightning@users.noreply.github.com>
2022-10-30 21:32:16 +0700

    Add files via upload

---
+++ Client/main.js
@@ -0,0 +1,128 @@
+const canvas = document.getElementById("canvas")
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+const ctx = canvas.getContext("2d")
+
+let nstack = []
+let pstack = []
+let curprim = []
+let fp = true
+
+let prevX = null
+let prevY = null
+
+ctx.lineWidth = 5
+
+let draw = false
+let enddraw = false
+
+let clrs = document.querySelectorAll(".clr")
+clrs = Array.from(clrs)
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.dataset.clr
+    })
+})
+
+let clearBtn = document.querySelector(".clear")
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+let saveBtn = document.querySelector(".save")
+saveBtn.addEventListener("click", () => {
+    let data = canvas.toDataURL("imag/png")
+    let a = document.createElement("a")
+    a.href = data
+    a.download = "sketch.png"
+    a.click()
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey) 
+    {
+        if (event.code == 'KeyZ') 
+        {
+            if (pstack.length != 0)
+            {
+                nstack.push(pstack.pop())
+                ctx.clearRect(0, 0, canvas.width, canvas.height)
+                let bufcolour = ctx.strokeStyle
+                for (let prim_opt of pstack) 
+                {
+                    let prim = prim_opt[1]
+                    ctx.strokeStyle = prim_opt[0]
+                    ctx.beginPath()
+                    for (let points of prim) 
+                    {
+                        ctx.lineTo(points[0], points[1])
+                        ctx.moveTo(points[0], points[1])
+                    }
+                    ctx.stroke()
+                }
+                ctx.strokeStyle = bufcolour
+            }
+            return
+        }
+        if (event.code == 'KeyX') 
+        {
+            if (nstack.length != 0)
+            {
+                let prim_opt = nstack.pop()
+                let prim = prim_opt[1]
+                let bufcolour = ctx.strokeStyle
+                pstack.push(prim)
+                ctx.strokeStyle = prim_opt[0]
+                ctx.beginPath()
+                let fpoint = prim[0]
+                ctx.moveTo(fpoint[0], fpoint[1])
+                let points
+                for (points of prim) 
+                {
+                    ctx.lineTo(points[0], points[1])
+                    ctx.moveTo(points[0], points[1])
+                }
+                ctx.stroke()
+                ctx.strokeStyle = bufcolour
+            }
+        }
+    }
+  }, false);
+
+window.addEventListener("mousedown", (e) => draw = true)
+window.addEventListener("mouseup", (e) => enddraw = true)
+
+window.addEventListener("mousemove", (e) => {
+    if(prevX == null || prevY == null || !draw)
+    {
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
+    if(enddraw)
+    {
+        draw = false
+        enddraw = false
+        prevX = e.clientX
+        prevY = e.clientY
+        fp = true
+        pstack.push([ctx.strokeStyle, curprim])
+        curprim = []
+        return 
+    }
+    let currentX = e.clientX
+    let currentY = e.clientY
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+    if(fp)
+    {
+        curprim.push([prevX, prevY])
+    }
+    curprim.push([currentX, currentY])
+    prevX = currentX
+    prevY = currentY
+    fp = false
+})
