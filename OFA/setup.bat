49e3bfaa913a4b85ff83b0908c11adca15107628
Robolightning <89173189+Robolightning@users.noreply.github.com>
2022-11-13 21:26:33 +0700

    Delete OFA directory

--- OFA/setup.bat
+++
@@ -1,5 +0,0 @@
-@cd /d %~dp0
-pip install -r requirements.txt
-mkdir checkpoints
-curl -o ./checkpoints/caption.pt https://ofa-beijing.oss-cn-beijing.aliyuncs.com/checkpoints/caption_base_best.pt
-@pause
