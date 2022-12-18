935272673503aab3192288386f7825353912fc8e
Robolightning <89173189+Robolightning@users.noreply.github.com>
2022-12-19 02:18:05 +0700

    Create ApplicationDbContext.cs

---
+++ Client/StableDraw_ASP.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StableDraw_ASP.NET.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
