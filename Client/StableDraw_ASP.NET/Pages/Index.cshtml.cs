ef556766c7943ba4e5cc4f18e72b2a9ad84d9a0c
Robolightning <89173189+Robolightning@users.noreply.github.com>
2022-12-20 03:46:55 +0700

    Add files via upload

--- Client/StableDraw_ASP.NET/Pages/Index.cshtml.cs
+++ Client/StableDraw_ASP.NET/Pages/Index.cshtml.cs
@@ -6,12 +6,10 @@ namespace StableDraw_ASP.NET.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
         public void OnGet()
         {
 
