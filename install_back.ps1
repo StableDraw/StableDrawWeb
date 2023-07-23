981e06274919e9220f6b29fbe6c1070c5ed72b37
onomylock <onomylock@outlook.com>
2023-07-23 20:55:19 +0700

    Revert "Backend repos"
    This reverts commit 863dc0db5d8704c52c3f33044691e0746e6f7b12.

--- install_back.ps1
+++ install_back.ps1
@@ -11,19 +11,19 @@ dotnet add package MailKit --version 4.0.0
 dotnet add package jquery.TypeScript.DefinitelyTyped --version 3.1.2
 dotnet add package AspNetCore.ReCaptcha --version 1.7.0
 Set-Location ..
-Write-Output D|xcopy /H /Y /C /R /S Client\Areas CLI\Areas
-Write-Output D|xcopy /H /Y /C /R /S Client\Migrations CLI\Migrations
-Write-Output D|xcopy /H /Y /C /R /S Client\Pages CLI\Pages
-Write-Output D|xcopy /H /Y /C /R /S Client\Services CLI\Services
-Write-Output F|xcopy /H /Y /C /R /S Client\appsettings.Development.json CLI\appsettings.Development.json
-Write-Output F|xcopy /H /Y /C /R /S Client\appsettings.json CLI\appsettings.json
-Write-Output F|xcopy /H /Y /C /R /S Client\appsettings.Production.json CLI\appsettings.Production.json
-Write-Output F|xcopy /H /Y /C /R /S Client\main.ts CLI\main.ts
-Write-Output F|xcopy /H /Y /C /R /S Client\Program.cs CLI\Program.cs
-Write-Output F|xcopy /H /Y /C /R /S Client\tsconfig.json CLI\tsconfig.json
-Write-Output D|xcopy /H /Y /C /R /S Client\ClientApp\public CLI\ClientApp\public
-Write-Output D|xcopy /H /Y /C /R /S Client\ClientApp\src CLI\ClientApp\src
-Remove-Item Client -Force -Recurse
-Rename-Item CLI Client
-Set-Location Client
+Write-Output D|xcopy /H /Y /C /R /S Client_test\Areas CLI\Areas
+Write-Output D|xcopy /H /Y /C /R /S Client_test\Migrations CLI\Migrations
+Write-Output D|xcopy /H /Y /C /R /S Client_test\Pages CLI\Pages
+Write-Output D|xcopy /H /Y /C /R /S Client_test\Services CLI\Services
+Write-Output F|xcopy /H /Y /C /R /S Client_test\appsettings.Development.json CLI\appsettings.Development.json
+Write-Output F|xcopy /H /Y /C /R /S Client_test\appsettings.json CLI\appsettings.json
+Write-Output F|xcopy /H /Y /C /R /S Client_test\appsettings.Production.json CLI\appsettings.Production.json
+Write-Output F|xcopy /H /Y /C /R /S Client_test\main.ts CLI\main.ts
+Write-Output F|xcopy /H /Y /C /R /S Client_test\Program.cs CLI\Program.cs
+Write-Output F|xcopy /H /Y /C /R /S Client_test\tsconfig.json CLI\tsconfig.json
+Write-Output D|xcopy /H /Y /C /R /S Client_test\ClientApp\public CLI\ClientApp\public
+Write-Output D|xcopy /H /Y /C /R /S Client_test\ClientApp\src CLI\ClientApp\src
+Remove-Item Client_test -Force -Recurse
+Rename-Item CLI Client_test
+Set-Location Client_test
 dotnet watch
