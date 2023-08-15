9eff30ece798ddc8e0322d847355f4dc61d03f7e
7Funny <sdastaj@gmail.com>
2023-08-15 18:45:44 +0700

    add CutterService

---
+++ Client/Controllers/CutterController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Drawing;
+using CLI.Services;
+
+
+namespace CLI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class CutterController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> CutterPhoto([FromBody] string body)
+        {
+            var client = new HttpClient();
+
+            RequestData requestData = JsonConvert.DeserializeObject<RequestData>(body);
+
+            Cutter cutterfun = new Cutter();
+
+            var sendReply = cutterfun.CutterFunc(requestData);
+
+            ReplyData replydata = new ReplyData()
+            {
+                Id = sendReply.Id,
+                Bitmap = sendReply.Bitmap
+            };
+
+            return Ok(replydata);
+        }
+    }
+
+
+
+
+}
