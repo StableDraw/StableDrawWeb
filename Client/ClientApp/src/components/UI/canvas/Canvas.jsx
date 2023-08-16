7880c7b296e7bed85a86835619a4cdf8dd0a0b07
RobinVanDutch <robinvandutch@mail.ru>
2023-08-16 23:12:03 +0700

    add State and Logic: Pencil.jsx , Eraser.jsx, ArrowButton.jsx, MyInput.jsx

--- Client/ClientApp/src/components/UI/canvas/Canvas.jsx
+++ Client/ClientApp/src/components/UI/canvas/Canvas.jsx
@@ -1,13 +1,34 @@
-﻿import React from 'react';
+﻿import React, {useEffect, useRef} from 'react';
 import cl from './Canvas.module.css'
-const Canvas = () => {
+import CanvasState from "../../../store/canvasState";
+import toolState from "../../../store/toolState";
+import Brush from "../../../tools/Brush";
+const Canvas = ({width, height}) => {
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        CanvasState.setCanvas(canvasRef.current);
+        toolState.setTool(new Brush(canvasRef.current))
+    }, []);
+
+    const mouseDownHandler = () => {
+        CanvasState.pushToUndo(canvasRef.current.toDataURL())
+    }
+
     return (
         <div className={cl.d_frame} id="d_frame">
-            <div className={cl.v_frame} id="v_frame">
-                <canvas className={cl.drawfield} id="canvas_additional" style={{ zIndex: 10 }}></canvas>
-                <canvas className={cl.drawfield} id="canvas_foreground" style={{ zIndex: 9 }}></canvas>
-                <canvas className={cl.drawfield} id="canvas_background" style={{ zIndex: 8 }}></canvas>
-                <div className={cl.drawfield} id="alpha_img" style={{ zIndex: 7, backgroundImage: "url(alpha_pattern.png)", backgroundRepeat: "repeat" }}></div>
+            <div className={cl.v_frame}  id="v_frame">
+                <canvas 
+                    onMouseDown={() => mouseDownHandler()} 
+                    ref={canvasRef} 
+                    width={width} 
+                    height={height} 
+                    // className={cl.drawfield} 
+                    id="canvas_background" 
+                    style={{ zIndex: 8 }}>
+                    
+                </canvas>
+                {/*<div className={cl.drawfield} id="alpha_img" style={{ zIndex: 7, backgroundImage: "url(alpha_pattern.png)", backgroundRepeat: "repeat" }}></div>*/}
             </div>
         </div>
     );
