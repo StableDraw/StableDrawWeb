55fcb89f4abdb98ecf709422e61bb17236237c92
Artem Fedchenko <artem_fedchenko_2017@mail.ru>
2023-08-23 22:00:43 +0700

    Сцена с холодильником оптимизирована

--- Client/ClientApp/src/components/babylon/SceneBar.jsx
+++ Client/ClientApp/src/components/babylon/SceneBar.jsx
@@ -8,7 +8,7 @@ import { useState, memo } from "react";
 const scenes = [
 	{ id: 1, name: "Box", img: '/babylon/imgPreviewScenes/box.png', scene: 'BoxFix2' },
 	{ id: 2, name: "Cart", img: '/babylon/imgPreviewScenes/Cart.jpeg', scene: 'CartFix' },
-	{ id: 3, name: "Fridge", img: '/babylon/imgPreviewScenes/Fridge.png', scene: 'Fridge' },
+	{ id: 3, name: "Fridge", img: '/babylon/imgPreviewScenes/Fridge.png', scene: 'FridgeFix' },
 ];
 
 
