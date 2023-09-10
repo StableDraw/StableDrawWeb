064fa347a7d9c7e795e90fb18236180b330e312f
onomylock <onomylock@outlook.com>
2023-09-10 23:34:39 +0700

    fix

--- StableDraw.NeuralService/src/app.py
+++ StableDraw.NeuralService/src/app.py
@@ -6,12 +6,17 @@ import aio_pika
 from neuralConsumer import LOG_FORMAT
 from reconnectingNeuralConsumer import ReconnectingExampleConsumer
 
-app = FastAPI()
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+amqp_url = 'rabbitmq://rmuser:rmpassword@localhost:5672/%2F'
+consumer = ReconnectingExampleConsumer(amqp_url)
+consumer.run()
 
-@app.on_event('startup')
-async def startup():
-    loop = asyncio.get_event_loop()
-    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
-    amqp_url = 'rabbitmq://rmuser:rmpassword@localhost:5672/%2F'
-    consumer = ReconnectingExampleConsumer(amqp_url)
-    consumer.run()
+# app = FastAPI()
+
+# @app.on_event('startup')
+# async def startup():
+#     loop = asyncio.get_event_loop()
+#     logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+#     amqp_url = 'rabbitmq://rmuser:rmpassword@localhost:5672/%2F'
+#     consumer = ReconnectingExampleConsumer(amqp_url)
+#     consumer.run()
