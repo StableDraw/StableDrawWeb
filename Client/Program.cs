4bd0fe9611fcfbf8ad0804164d97124cd6c55624
maxoncheeg <maxoncheeg@gmail.com>
2023-09-18 21:52:21 +0700

    Added a few methods

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
 using System.Net;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using CLI.Services;
 using CLI.Extensions;
-using CLI.Settings;
 using GreenPipes;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using StableDraw.Core.Models;
-using StableDraw.Domain.Data.Identity;
 using StableDraw.Domain.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,51 +13,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDatabases();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddIdentityServer().AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+builder.Services.AddApplicationUserIdentity();//extensions
 
 builder.Services.AddTransient<IEmailSender, EmailSender>();
 
-builder.Services.AddScoped<UserManager<ApplicationUser>>();
-
-builder.Services.AddAuthentication().AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-}).AddOAuth("VK", "VKontakte", vkontakteOptions =>
-{
-    vkontakteOptions.ClientId = builder.Configuration["Authentication:VKontakte:ClientId"];
-    vkontakteOptions.ClientSecret = builder.Configuration["Authentication:VKontakte:ClientSecret"];
-    vkontakteOptions.ClaimsIssuer = "VKontakte";
-    vkontakteOptions.CallbackPath = new PathString("/signin-vkontakte");
-    vkontakteOptions.AuthorizationEndpoint = "https://oauth.vk.com/authorize";
-    vkontakteOptions.TokenEndpoint = "https://oauth.vk.com/access_token";
-    vkontakteOptions.Scope.Add("email");
-    vkontakteOptions.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "user_id");
-    vkontakteOptions.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
-    vkontakteOptions.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "first_name");
-    vkontakteOptions.ClaimActions.MapJsonKey(ClaimTypes.Surname, "last_name");
-    vkontakteOptions.SaveTokens = true;
-    vkontakteOptions.Events = new OAuthEvents
-    {
-        OnCreatingTicket = context =>
-        {
-            context.RunClaimActions(context.TokenResponse.Response.RootElement);
-            return Task.CompletedTask;
-        },
-        OnRemoteFailure = OnFailure
-    };
-});
-Task OnFailure(RemoteFailureContext arg)
-{
-    Console.WriteLine(arg);
-    return Task.CompletedTask;
-}
+builder.Services.AddGoogleAuthentication(builder.Configuration);//extension
 
 builder.Services.AddAuthentication().AddIdentityServerJwt();
-
 builder.Services.AddAuthorization();
 
 builder.Services.AddControllersWithViews();
@@ -71,35 +27,11 @@ builder.Services.AddControllers();
 
 builder.Services.AddRazorPages();
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 0;
-
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
-
-    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = true;
-
-    options.SignIn.RequireConfirmedEmail = true;
-});
+builder.Services.ConfigureIdentityOptions();//extension
 
 //builder.Services.Configure<DataProtectionTokenProviderOptions>(options => options.TokenLifespan = TimeSpan.FromHours(5));
 
-builder.Services.AddHsts(options =>
-{
-    options.Preload = true;
-    options.IncludeSubDomains = true;
-    options.MaxAge = TimeSpan.FromDays(60);
-    options.ExcludedHosts.Add("stabledraw.com");
-    options.ExcludedHosts.Add("www.stabledraw.com");
-});
+builder.Services.AddConfiguredHSTS();
 
 builder.Services.AddHttpsRedirection(options =>
 {
@@ -108,8 +40,7 @@ builder.Services.AddHttpsRedirection(options =>
 });
 
 // recaptcha
-builder.Services.Configure<GoogleRecaptchaSettings>(builder.Configuration.GetSection("GoogleRecaptcha"));
-builder.Services.AddTransient<GoogleRecaptchaService>();
+builder.Services.AddGoogleRecaptcha(builder.Configuration);//extension
 
 // rabbitMQ
 builder.Services.AddMassTransit(cfg =>
