d1e7044947e5d29719c61748fcee5c92057b361a
maxoncheeg <maxoncheeg@gmail.com>
2023-09-19 20:17:05 +0700

    another try

---
+++ Client/Extensions/PaymentExtension.cs
@@ -0,0 +1,22 @@
+﻿using CLI.Services;
+using CLI.Settings;
+using StableDraw.Domain.Repositories;
+
+namespace CLI.Extensions;
+
+public static class PaymentExtension
+{
+    //public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    //{
+    //    services.Configure<PaymentSettings>(configuration);
+    //}
+
+    //public static void AddHttpClientServices(this IServiceCollection services)
+    //{
+    //    services.AddHttpClient<IPaymentService, YookassaService>(client =>
+    //    {
+    //        client.DefaultRequestHeaders.Add("key", "value");
+    //    });
+    //    services.AddScoped<IPaymentRepository, FakePaymentRepository>();
+    //}
+}
