a4f17016ddf904b8b96f7d81014eb76231791a9f
onomylock <onomylock@outlook.com>
2023-10-05 18:02:14 +0700

    feat: init new client project

---
+++ StableDraw.Client/Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Data;
+
+public class ApplicationContext: IdentityDbContext<ApplicationUser>
+{
+    private readonly IConfiguration _configuration;
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> option, IConfiguration configuration) : base(option)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(_configuration.GetConnectionString("DefaultConnection"), o => o.MigrationsHistoryTable(
+            tableName: HistoryRepository.DefaultTableName,
+            schema: "AppUser"));
+    }
+}
