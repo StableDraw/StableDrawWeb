d5e0d11bc5538d5622e9e5d5dc8be44e3b57daf9
Robolightning <89173189+Robolightning@users.noreply.github.com>
2023-11-15 12:53:08 +0700

    fix

--- StableDraw.Domain/Repositories/IImageRepository.cs
+++ StableDraw.Domain/Repositories/IImageRepository.cs
@@ -8,7 +8,7 @@ public interface IImageRepository : IBaseRepository<Image>
     Task<IEnumerable<Image>> GetImagesAsync(string userId);
     Task<IEnumerable<Guid>> CreateImagesAsync(IEnumerable<(string, string)> imageNames, string userId);
     Task DeleteImagesAsync(IEnumerable<string> imageNames, string userId);
-    void CreateImage(Image image);
+    Task CreateImage(Image image);
     Task<Image?> GetImage(string imageName, string userId);
     Task DeleteImage(string imageName, string userId);
 }
